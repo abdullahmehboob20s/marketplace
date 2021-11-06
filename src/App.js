615fe4947fc1afafa133477d706c91b46a4b68d5
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2021-11-06 19:52:03 +0500

    a

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "pages/Home/Home";
 import PetDetails from "pages/PetDetails/PetDetails";
+import ScrollToTop from "components/ScrollToTop/ScrollToTop";
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/pet-details" component={PetDetails} />
